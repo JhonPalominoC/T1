d4dafe80a89bb5ce1f2e6afb3c18f90c97f62635
JhonPalominoC <142623871+JhonPalominoC@users.noreply.github.com>
2023-09-08 13:54:33 -0500

    Add files via upload

---
+++ src/T1/T1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package T1;
+
+/**
+ *
+ * @author emers
+ */
+public class T1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        
+        
+    }
+    
+}
